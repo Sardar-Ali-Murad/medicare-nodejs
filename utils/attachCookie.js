0445140666fe6906185f1b7262c9130735feae61
Sardar-Ali-Murad <mar389786@gmail.com>
2023-11-08 15:06:57 +0500

    fixing cookie not showing try#1

--- utils/attachCookie.js
+++ utils/attachCookie.js
@@ -1,10 +1,13 @@
 const attachCookie = ({ res, token }) => {
-  const oneDay = 1000 * 60 * 60 * 24;
+  // const oneDay = 1000 * 60 * 60 * 24;
 
   res.cookie('token', token, {
     httpOnly: true,
-    expires: new Date(Date.now() + oneDay),
-    secure: process.env.NODE_ENV === 'production',
+    // expires: new Date(Date.now() + oneDay),
+    // secure: process.env.NODE_ENV === 'production',
+    // httpOnly: true,
+    path: "/",
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30days
   });
 };
 
